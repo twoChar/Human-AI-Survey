261a5fed94b175b3b06936e380f117c15109928a
twoChar <tushar20254@iiitd.ac.in>
2026-01-06 16:42:38 +0530

    reoo

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  basePath: '/Human-AI-Survey',
+  assetPrefix: '/Human-AI-Survey',
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
